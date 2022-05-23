ccf9db9da1ddb14e4ac0de71185f3f27c81a4342
Carla Serrão <carla.suporteam@gmail.com>
2022-05-23 15:34:09 -0400

    adicinonando um console log

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
-console.log("Rodando o sistema de gerenciamento de jogos")
-console.log("Aplicacão está se conectando ao banco de dados")
+console.log('Rodando o sistema de gerenciamento de jogos')
+console.log('Aplicacão está se conectando ao banco de dados')
+// mais console log de teste:
+console.log('A Carla fez esse commit sem você ver na aula!')
+console.log('Enviando para o GitHub')
