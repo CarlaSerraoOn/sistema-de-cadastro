cbc8b5a3016e8e8318762ab67f2d1f9be25c6480
Carla Serrão <carla.suporteam@gmail.com>
2022-05-23 15:44:39 -0400

    linkando o app js com o html

--- app.js
+++ app.js
@@ -1,5 +1,2 @@
-console.log('Rodando o sistema de gerenciamento de jogos')
-console.log('Aplicacão está se conectando ao banco de dados')
-// mais console log de teste:
-console.log('A Carla fez esse commit sem você ver na aula!')
-console.log('Enviando para o GitHub')
+console.log("Rodando o sistema de gerenciamento de jogos")
+console.log("Aplicacão está se conectando ao banco de dados")
